db17ee4502ccb07d1ca84f23be0f537756615e5a
Nima HeydariNasab <nima.heydari79@yahoo.com>
2019-08-15 12:04:09 +0000

    Apply fixes from StyleCI

--- example.php
+++ example.php
@@ -3,8 +3,7 @@
 /**
  * This file is a part of FontAwesome Pro Downloader
  * By NimaH79
- * http://nimah79.ir
+ * http://nimah79.ir.
  */
-
 $fontawesome_downloader = new FontAwesomeDownloader(FontAwesomeDownloader::LATEST_VERSION);
 $fontawesome_downloader->download();
